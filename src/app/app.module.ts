0d6704a55232513ddf6bf5affa258bc21d32e04d
mojzesz13 <bozejewski@gmail.com>
2021-04-07 22:19:06 +0200

    structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './pages/header.component/header.component';
+import { RecipesComponent } from './pages/recipes/recipes.component';
+import { RecipeListComponent } from './pages/recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './pages/recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './pages/recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShopingListComponent } from './pages/shoping-list/shoping-list.component';
+import { ShopingEditComponent } from './pages/shoping-list/shoping-edit/shoping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShopingListComponent,
+    ShopingEditComponent
   ],
   imports: [
     BrowserModule,
